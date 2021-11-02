cae686da6aa83556dad8938b292c226d2ba16d0f
Tianyuan Zheng <stella@Tianyuans-MacBook-Pro.local>
2021-11-01 20:31:15 -0400

    add basic profile

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,8 @@
 class DoctorsController < ApplicationController
+    before_action :authenticate_doctor!
     def profile
+        if doctor_signed_in?
+            @doctor = current_doctor
+        end
     end
 end
