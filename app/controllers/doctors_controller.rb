a8d33277befef27ca1e43343c99919179fdf1c58
Tianyuan Zheng <stella@Tianyuans-MacBook-Pro.local>
2021-11-02 16:56:18 -0400

    update editinfo

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -14,4 +14,19 @@ class DoctorsController < ApplicationController
         @doctor = current_doctor
     end
 
+    def update
+        @doctor = current_doctor
+        @doctor.update(update_params)
+        flash[:notice] = "#{@doctor.username} profile was successfully updated."
+        redirect_to "/doctors/#{@doctor.id}/profile"
+    end
+    
+
+    private
+    # Making "internal" methods private is not required, but is a common practice.
+    # This helps make clear which methods respond to requests, and which ones do not.
+    def update_params
+      params.require(:doctor).permit(:last_name, :first_name, :gender, :birthday)
+    end
+    
 end
