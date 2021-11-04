faaf4d92549caefeccbcae13bc3110ef59dd1638
Tianyuan Zheng <stella@Tianyuans-MacBook-Pro.local>
2021-11-03 20:01:17 -0400

    update param

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -26,7 +26,7 @@ class DoctorsController < ApplicationController
     # Making "internal" methods private is not required, but is a common practice.
     # This helps make clear which methods respond to requests, and which ones do not.
     def update_params
-      params.require(:doctor).permit(:last_name, :first_name, :gender, :birthday)
+      params.require(:doctor).permit(:last_name, :first_name, :department, :gender, :birthday)
     end
     
 end
