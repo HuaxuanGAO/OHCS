807e6c9da94a47b8da1106da8b0fe39561b466fc
Tianyuan Zheng <stella@Tianyuans-MacBook-Pro.local>
2021-11-03 20:01:17 -0400

    update param

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   get '/rooms/:id/chat', to: 'rooms#room'
   devise_for :doctors
   get '/doctors/:id/profile', to: 'doctors#profile' 
+  get '/doctors', to: 'doctors#index' 
+  get '/doctors/editinfo', to: 'doctors#editinfo' 
+  put '/doctors/update', to: 'doctors#update'
   devise_for :patients
   get '/patients/:id/profile', to: 'patients#profile'
   get '/patients', to:'patients#index'
   get '/patients/editinfo', to: 'patients#editinfo'
   put '/patients/update', to: 'patients#update'
-  get '/doctors', to: 'doctors#index' 
-  get '/doctors/editinfo', to: 'doctors#editinfo' 
-  put '/doctors/update', to: 'doctors#update'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root controller: :patients, action: :login
