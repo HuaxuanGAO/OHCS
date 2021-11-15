9a5cf8b017b1e6d02248ec618b015b34d49e5c33
Huaxuan Gao <gogoghx@163.com>
2021-11-15 15:10:23 -0500

    change routes from p/d to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,7 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
         if current_user
-            "/patients/#{current_user.id}/profile" # your path
-        # elsif current_doctor
-        #     "/doctors/#{current_doctor.id}/profile"
+            "/users/#{current_user.id}/profile" # your path
         else
             "/"
         end
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,10 @@ Rails.application.routes.draw do
   resources :rooms
   get '/dashboard', to: 'rooms#dashboard', as: 'dashboard'
   get '/rooms/:id/chat', to: 'rooms#room'
-  get '/doctors/:id/profile', to: 'doctors#profile' 
-  get '/doctors', to: 'doctors#index' 
-  get '/doctors/editinfo', to: 'doctors#editinfo' 
-  put '/doctors/update', to: 'doctors#update'
-  get '/patients/:id/profile', to: 'patients#profile'
-  get '/patients', to:'patients#index'
-  get '/patients/editinfo', to: 'patients#editinfo'
-  put '/patients/update', to: 'patients#update'
+  get '/users/:id/profile', to: 'users#profile' 
+  get '/users', to: 'users#index' 
+  get '/users/editinfo', to: 'users#editinfo' 
+  put '/users/update', to: 'users#update' 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
