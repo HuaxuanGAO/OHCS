82264d9d676a12262e11885fef2cffc7cf1c552e
Tianyuan Zheng <stella@Tianyuans-MacBook-Pro.local>
2021-11-17 22:37:59 -0500

    update doctor class

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,12 +2,12 @@ class Doctor < ApplicationRecord
     belongs_to :user
 
     def self.getallnames
-        names = []
+        namepairs = {}
         Doctor.all.each do |doctor|
             user = User.find_by(id: doctor.user_id)
             name = user.first_name+" "+user.last_name
-            names = names + [name]
+            namepairs[name] = doctor.user_id
         end
-        return names
+        return namepairs
     end
 end
