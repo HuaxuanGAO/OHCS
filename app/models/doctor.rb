866d6133dd0eef324f2bf186484dd6bff00a303f
HuaxuanGAO <33063662+HuaxuanGAO@users.noreply.github.com>
2021-11-17 22:06:12 -0500

    Revert "Iter2"

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,12 +2,12 @@ class Doctor < ApplicationRecord
     belongs_to :user
 
     def self.getallnames
-        namepairs = {}
+        names = []
         Doctor.all.each do |doctor|
             user = User.find_by(id: doctor.user_id)
             name = user.first_name+" "+user.last_name
-            namepairs[name] = doctor.user_id
+            names = names + [name]
         end
-        return namepairs
+        return names
     end
 end
